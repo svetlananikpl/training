7b1f4c351876b092b3ff0088d7f3940e639c34fc
Svetlana Nikonova-Plevko <svetlananikpl@gmail.com>
2016-05-19 16:07:15 +0300

    add home page

--- public/app/app.js
+++ public/app/app.js
@@ -25,7 +25,7 @@ myApp.config(function ($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
     $stateProvider
         .state('home', {
-            url: '/',
+            url: '/home',
             templateUrl: 'templates/home.html',
             controller: 'violationsCtrl'
         })
